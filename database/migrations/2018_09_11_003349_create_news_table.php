bfb47bf533d22146b1b8d9293082393d71ece0b8
DrMaxis <Grandmasterantwii@gmail.com>
2018-09-10 23:10:22 -0400

    upd

---
+++ database/migrations/2018_09_11_003349_create_news_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->string('info1')->nullable();
+            $table->string('info2')->nullable();
+            $table->string('info3')->nullable();
+            $table->string('media')->nullable();
+            $table->string('applemusic')->nullable();
+            $table->string('spotify')->nullable();
+            $table->string('soundcloud')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -1,69 +1,81 @@
-@extends('layouts.ui')
+@extends('layouts.ui') 
 @section('content')
 
-        <section class="music-posts section infinite" data-next-page="{{$news->nextPageUrl()}}">
-                <div class="container">
-                        <div class="newsContainer" id="newsData">
+<section class="music-posts section infinite" data-next-page="{{$news->nextPageUrl()}}">
+    <div class="container">
+        <div class="newsContainer" id="newsData">
 
-            @if(count($news) > 0 )
-            @foreach($news as $article)
-                <div class="newsArticle">
-                    
-                        <div class="newsContent">
-                         
+            @if(count($news) > 0 ) @foreach($news as $article)
+            <div class="newsArticle">
 
-                            <div class="newsImage">
-                                
-                                    
-
-                                           
-                                 
-                            
-                                   
-{{-- {{asset("storage/images/postimages/$article->postimage")}} --}}
-                                   <div>
-                                       
-                                          <iframe width="100%" height="100%" src="{{$article->sourcelink}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                                      
-
-                                        
-                                   </div>
-                            
-
-                            </div>
+                <div class="newsContent">
 
 
-            
-            <div class ="newsInfo">
-                    <h4 class="title"><p><b>{{$article->title}}</b></p></h4>
-                    <p>
-                        {{$article->info}}
-                    </p>
-            </div>
+                    <div class="newsMedia">
+                        <div>
+
+                            <iframe class="newsFrame" width="500px" height="500px" src="{{$article->media}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                         </div>
                     </div>
-                
-            
-        
-        @endforeach
-                @else <p> no posts found</p>
-            @endif
-                </div>
-            </div>
-           
 
-            <div class="jax">
 
-            </div>
-        </section>
+
+                    <div class="newsInfo">
+                        <h4 class="title">
+                            <p>
+                                <b>
+                                        {{$article->title}}
+                                </b>
+                            </p>
+                        </h4>
+@if(!empty($article->info1))
     
 
+                        <p>
+                                {{$article->info1}}
 
-      
+                        </p>
+ @endif
 
-        
+@if(!empty($article->info2))
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script  language="JavaScript" type="text/javascript" src="{{asset('js/jsdepends/musicajax.js')}}"></script> 
+                        <p>
+                            {{$article->info2}}
+                            
+                    </p>
+@endif
 
+@if(!empty($article->info3))
+                    <p>
+                        {{$article->info3}}
+                        
+                </p>
+@endif
+                        
+                    </div>
+                </div>
+            </div>
+
+
+
+            @endforeach @else
+            <p> no posts found</p>
+            @endif
+        </div>
+    </div>
+
+
+    <div class="jax">
+
+    </div>
+</section>
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script language="JavaScript" type="text/javascript" src="{{asset('js/jsdepends/musicajax.js')}}"></script>
 @endsection
